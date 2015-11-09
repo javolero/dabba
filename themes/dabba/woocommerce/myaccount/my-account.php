36ec9e0cec1feeb24c77290e025598ae2a983561
Raúl De Zamacona <raul@zamacona.me>
2015-11-09 16:42:28 -0600

    my account

--- themes/dabba/woocommerce/myaccount/my-account.php
+++ themes/dabba/woocommerce/myaccount/my-account.php
@@ -15,19 +15,7 @@ wc_print_notices(); ?>
 
 <div class="[ container ]">
 
-	<p class="[ myaccount_user ][ margin-top--large ]">
-		<?php
-		printf(
-			__( 'Hello <strong>%1$s</strong> (not %1$s? <a class="[ color-primary ]" href="%2$s">Sign out</a>).', 'woocommerce' ) . ' ',
-			$current_user->display_name,
-			wc_get_endpoint_url( 'customer-logout', '', wc_get_page_permalink( 'myaccount' ) )
-		);
-
-		printf( __( 'From your account dashboard you can view your recent orders, manage your shipping and billing addresses and <a class="[ color-primary ]" href="%s">edit your password and account details</a>.', 'woocommerce' ),
-			wc_customer_edit_account_url()
-		);
-		?>
-	</p>
+	<h2 class="[ text-center ][ margin-top-bottom ]">Mi cuenta</h2>
 
 	<?php do_action( 'woocommerce_before_my_account' ); ?>
 
@@ -35,7 +23,31 @@ wc_print_notices(); ?>
 
 	<?php wc_get_template( 'myaccount/my-orders.php', array( 'order_count' => $order_count ) ); ?>
 
+	<h3 class="[ text-left ][ margin-bottom--large ]">
+		<a class="[ color-dark ]" role="button" data-toggle="collapse" href="#ordenesProgramadas" aria-expanded="false" aria-controls="collapseExample">
+			Ordenes programadas
+		</a>
+	</h3>
+
+	<hr class="[ divider-primary ][ margin-bottom-large ]">
+
 	<?php wc_get_template( 'myaccount/my-address.php' ); ?>
+
+	<hr class="[ divider-primary ][ margin-bottom-large ]">
+
+	<div class="[ myaccount_user ][ margin-top-bottom--large ][ text-center ]">
+		<?php
+			printf( __( '<a class="[ color-primary ][ btn btn-primary btn-hollow ][ margin-sides--small ]" href="%s">editar detalles</a>', 'woocommerce' ),
+				wc_customer_edit_account_url()
+			);
+
+			printf(
+				__( '<a class="[ color-primary ][ btn btn-primary btn-hollow ][ margin-sides--small ]" href="%2$s">Sign out</a>', 'woocommerce' ) . ' ',
+				$current_user->display_name,
+				wc_get_endpoint_url( 'customer-logout', '', wc_get_page_permalink( 'myaccount' ) )
+			);
+		?>
+	</div>
 
 	<?php do_action( 'woocommerce_after_my_account' ); ?>
 
