48aaa27c3293944819c6a54f1463033af1037b56
maichomper <maichomper@gmail.com>
2015-11-26 22:53:03 -0600

    quitar warning de my programmed orders

--- themes/dabba/woocommerce/myaccount/my-programmed-orders.php
+++ themes/dabba/woocommerce/myaccount/my-programmed-orders.php
@@ -1,6 +1,6 @@
 <?php
 $customer_orders = get_posts( apply_filters( 'woocommerce_my_account_my_orders_query', array(
-	'numberposts' => $order_count,
+	'numberposts' => '-1',
 	'meta_key'    => '_customer_user',
 	'meta_value'  => get_current_user_id(),
 	'post_type'   => wc_get_order_types( 'view-orders' ),
